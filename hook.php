8c83dd9e70263ed67bbad5dc50757860fa56e382
”zhenya091992” <zhenya091992@gmail.com>
2022-03-29 18:50:32 +0300

    telegramBot

--- hook.php
+++ hook.php
@@ -4,10 +4,11 @@ require __DIR__ . '/vendor/autoload.php';
 
 $bot_api_key  = '778407765:AAGfqcbwfHbgAlD9IGYzrBFJka0mUfYJcvQ';
 $bot_username = 'Zaebot12345_bot';
+$botexplanation_username = 'zaebot';
 
 try {
     // Create Telegram API object
-    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
+    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $botexplanation_username);
 
     // Handle telegram webhook request
     $telegram->handle();
--- set.php
+++ set.php
@@ -6,10 +6,11 @@ require __DIR__ . '/vendor/autoload.php';
 $bot_api_key = '778407765:AAGfqcbwfHbgAlD9IGYzrBFJka0mUfYJcvQ';
 $bot_username = 'Zaebot12345_bot';
 $hook_url = 'https://35.184.170.195/bot/telegram_bot.php';
+$bot_usernameexplanation = 'это бот';
 
 try {
     // Create Telegram API object
-    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
+    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_usernameexplanation);
 
     // Set webhook
     $result = $telegram->setWebhook($hook_url,['certificate' => '/etc/apache2/sites-available/default-ssl.conf']);
