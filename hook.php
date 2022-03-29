4937b9dad59f8291ef8b20cbf30a6c2fb7b36256
”zhenya091992” <zhenya091992@gmail.com>
2022-03-29 19:09:51 +0300

    telegramBot

--- hook.php
+++ hook.php
@@ -9,11 +9,11 @@ $botexplanation_username = 'zaebot';
 try {
     // Create Telegram API object
     $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $botexplanation_username);
-
+    echo 'ok';
     // Handle telegram webhook request
     $telegram->handle();
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
     // Silence is golden!
     // log telegram errors
-    // echo $e->getMessage();
+     echo $e->getMessage();
 }
